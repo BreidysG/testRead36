d38b2572a5bd7c4f7d9675059e2b6ae653415107
BreidysG <Breidysgutierrez8@gmail.com>
2024-11-01 11:01:04 -0400

    Primer commit

---
+++ script.js
@@ -0,0 +1,196 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const titleInit = document.getElementById("titleInit");
+    const paragraphInit = document.getElementById("paragraphInit");
+    const startButton = document.getElementById('startButton');
+    const textContainer = document.getElementById('textContainer');
+    const finishButton = document.getElementById('finishButton');
+    const questionnaire = document.getElementById('questionnaire');
+    const quizForm = document.getElementById('quizForm');
+    const results = document.getElementById('results');
+    const timerValue = document.getElementById('timerValue');
+    let time = [];
+
+    let wordCount = 0;
+    let startTime, endTime;
+    let quizCompleted = false;
+    let timerInterval;
+
+    // Texto para las preguntas del cuestionario
+    const questions = [
+        {
+            question: "¿En qué isla se desarrolla la lectura?",
+            answers: [
+                "Puerto Rico",
+                "Hawaii",
+                "Sri Lanka",
+                "Grecia"
+            ],
+            correctAnswer: "Sri Lanka"
+        },
+        {
+            question: "¿Cuál es la idea principal de la lectura?",
+            answers: [
+                "El relato de la tradición de la ceremonia en Sri Lanka del corte del agua",
+                "La vivencia de un niño con la leyenda del agua y la espada",
+                "La importancia del agua en Sri Lanka",
+                "La leyenda antigua de la bendición del agua"
+            ],
+            correctAnswer: "El relato de la tradición de la ceremonia en Sri Lanka del corte del agua"
+        },
+        {
+            question: "¿Qué llevan los monjes hacia el río aparte de la espada?",
+            answers: [
+                "Cuatro vasijas de barro",
+                "Una vasija de barro",
+                "Cuatro vasijas de metal",
+                "Una bandera y cuatro platones de plástico"
+            ],
+            correctAnswer: "Cuatro vasijas de metal"
+        },
+        {
+            question: "¿Cuál es el mensaje principal de la lectura?",
+            answers: [
+                "Que el agua es un líquido muy preciado para todos",
+                "Que el agua no se debe tomar como algo eterno y que es propiedad de todos",
+                "Que con el agua no se debe jugar",
+                "Que el agua es saludable"
+            ],
+            correctAnswer: "Que el agua no se debe tomar como algo eterno y que es propiedad de todos"
+        },
+        {
+            question: "¿Qué se hace con las vasijas de metal al terminar la ceremonia?",
+            answers: [
+                "Se lanzan al río",
+                "Se llenan con el agua del río y se regresan al templo",
+                "Se devuelven a la tienda del monje",
+                "Se dejan junto con la espada en el río"
+            ],
+            correctAnswer: "Se llenan con el agua del río y se regresan al templo"
+        }
+    ];
+    
+    
+    
+    
+    
+    // Función para contar las palabras en el texto
+    function countWords(text) {
+        return text.split(/\s+/).length;
+    }
+
+    // Función para calcular la velocidad de lectura (palabras por minuto)
+    function calculateReadingSpeed(startTime, endTime, wordCount) {
+        const minutes = (endTime - startTime) / 60000; // Convertir a minutos
+        return Math.round(wordCount / minutes);
+    }
+
+    // Función para calcular el tiempo por palabra en milisegundos
+    function calculateTimePerWord(ppm) {
+        return Math.round(60000 / ppm); // ms por palabra
+    }
+
+    // Función para mostrar las preguntas del cuestionario
+    function displayQuestions() {
+        const questionList = document.getElementById('questionList');
+        questionList.innerHTML = '';
+        
+        questions.forEach((question, index) => {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `
+                <p>${question.question}</p>
+                <div class="question-options">
+                    ${question.answers.map((answer, i) => `
+                        <label>
+                            <input type="radio" name="answer${index}" value="${answer}">
+                            ${answer}
+                        </label>
+                    `).join('')}
+                </div>
+            `;
+            questionList.appendChild(listItem);
+        });
+    }
+
+    // Evento para comenzar el test al hacer clic en "Empezar"
+    startButton.addEventListener('click', function() {
+        textContainer.classList.remove('hidden');
+        paragraphInit.classList.add("hidden");
+        startButton.classList.add('hidden');
+        startTime = Date.now();
+        // Iniciar el contador
+        timerInterval = setInterval(updateTimer, 1000);
+    });
+
+    // Función para actualizar el contador de tiempo
+    function updateTimer() {
+        const currentTime = Math.floor((Date.now() - startTime) / 1000); // Tiempo transcurrido en segundos
+        timerValue.textContent = currentTime;
+        time.push(currentTime);
+    }
+
+    // Evento para finalizar el test al hacer clic en "Terminar Test"
+    finishButton.addEventListener('click', function() {
+        clearInterval(timerInterval); // Detener el contador
+        titleInit.classList.add("hidden");
+        textContainer.classList.add('hidden');
+        finishButton.classList.add('hidden');
+        endTime = Date.now();
+        wordCount = countWords(document.getElementById('textToRead').textContent);
+        const readingSpeed = calculateReadingSpeed(startTime, endTime, wordCount);
+        questionnaire.classList.remove('hidden');
+        displayQuestions();
+    });
+
+    // Evento para enviar el cuestionario
+    quizForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        if (!quizCompleted) {
+            let anyAnswerSelected = true;
+
+            questions.forEach((question, index) => {
+                const selectedAnswer = document.querySelector(`input[name="answer${index}"]:checked`);
+                if (!selectedAnswer) {
+                    anyAnswerSelected = false;
+                    return;
+                }
+            });
+
+            if (!anyAnswerSelected) {
+                alert("Debes seleccionar una opción para cada pregunta antes de terminar.");
+                return;
+            }
+
+            let correctAnswers = 0;
+            let totalQuestions = questions.length;
+
+            questions.forEach((question, index) => {
+                const selectedAnswer = document.querySelector(`input[name="answer${index}"]:checked`);
+                if (selectedAnswer && selectedAnswer.value === question.correctAnswer) {
+                    correctAnswers++;
+                }
+            });
+
+            let comprehensionPercentage = (correctAnswers / totalQuestions) * 100;
+            let timeResult = time[time.length-1];
+
+            const readingSpeed = calculateReadingSpeed(startTime, endTime, wordCount); // Usar la velocidad de lectura real
+            const timePerWordInMs = calculateTimePerWord(readingSpeed); // Ajustar el tiempo por palabra basado en la velocidad de lectura real
+            
+            quizCompleted = true;
+            results.classList.remove('hidden');
+            questionnaire.classList.add('hidden');
+            document.getElementById('wordCountValue').textContent = wordCount;
+            document.getElementById('readingSpeedValue').textContent = `${readingSpeed} palabras por minuto`;
+            document.getElementById('comprehensionValue').textContent = `${comprehensionPercentage}`;
+            document.getElementById('timeResultValue').textContent = `${timeResult} segundos`;
+
+            // Mostrar tiempo por palabra ajustado
+            document.getElementById('results').innerHTML += `
+                <p class="lastParagrah">Toma nota de tu velocidad de lectura para poder realizar ajustes en los próximos ejercicios.</p>
+                <p class="finalMessage">Puedes salir y pasar a la siguiente clase.</p>
+                <p>Ajusta esta velocidad en tus ejercicios: ${timePerWordInMs} milisegundos por palabra</p>
+            `;
+        }
+    });
+});
+
